679b73fe1d7454962ace94ffde0ce8ccd77081f4
ychy0006 <141913601+ychy0006@users.noreply.github.com>
2023-08-25 11:59:38 +0900

    programmers 7,8

---
+++ AlgorithmCodeKata/Problem7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata
+{
+    internal class Problem7
+    {
+        static public int[] solution(long n)
+        {
+            long a = n;
+            int i = 0;
+            while (a > 0)
+            {
+                a /= 10;
+                i++;
+            }
+            a = n;
+            int[] answer = new int[i];
+            for (int j = 0; j < i; j++)
+            {
+                long ans = a % 10; // 자동 형변환
+                answer[j] = (int)ans; // 명시적 형변환해줘야함
+                a /= 10;
+            }
+            return answer;
+        }
+    }
+}
---
+++ AlgorithmCodeKata/Problem8.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata
+{
+    internal class Problem8
+    {
+        static public int solution(string s)
+        {
+            int num;
+            if (s[0] == '-' || s[0] == '+')
+            {
+                num = int.Parse(s.Substring(1, s.Length - 1));
+                if (s[0] == '-') num *= (-1);
+            }
+            else
+            {
+                num = int.Parse(s);
+            }
+            return num;
+        }
+    }
+}
