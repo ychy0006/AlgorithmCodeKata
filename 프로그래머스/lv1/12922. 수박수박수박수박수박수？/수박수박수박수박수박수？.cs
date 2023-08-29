fa6a6507c78af47a98da4124198a47556eff02eb
ychy0006 <141913601+ychy0006@users.noreply.github.com>
2023-08-30 08:36:18 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.19 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public string solution(int n) {
+        string answer = "";
+        for(int i=0;i<n;i++){
+            if(i%2==0) answer=answer+"수";
+            else answer=answer+"박";
+        }
+        return answer;
+    }
+}
