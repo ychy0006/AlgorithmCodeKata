7f5d64959a9bfe17ccea65a9ac545cb99bf17d23
ychy0006 <141913601+ychy0006@users.noreply.github.com>
2023-09-04 07:09:05 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.80 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
@@ -1,17 +1,8 @@
 using System;
+using System.Linq;
 
 public class Solution {
     public int solution(int[] numbers) {
-        int answer = 45;
-        int index=0;
-        System.Array.Sort(numbers);
-        for(int n=0;n<10;n++){
-            if(numbers[index]==n){
-                answer-=n;
-                index++;
-                if(numbers.Length==index) break;
-            }
-        }
-        return answer;
+        return 45- numbers.Sum();
     }
 }
