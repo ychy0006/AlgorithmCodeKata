7399045c152d148a5d95c6f63f4d74e58c9e6a24
ychy0006 <141913601+ychy0006@users.noreply.github.com>
2023-09-04 07:42:54 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 3.31 ms, Memory: 30.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cs
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cs
@@ -1,9 +1,7 @@
+using System.Linq;
+
 public class Solution {
     public string solution(string s) {
-        char[] arr=s.ToCharArray();
-        System.Array.Sort(arr);
-        System.Array.Reverse(arr);
-        string answer = new string(arr);//arr.ToString(); 하니까 "System.Char[]"이 결과값
-        return answer;
+        return new string(s.ToCharArray().OrderByDescending(x => x).ToArray());
     }
 }
